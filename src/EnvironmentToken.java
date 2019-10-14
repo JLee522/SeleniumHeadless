1e9d227ce975d38818acced6103cb3fbf2dbc3b5
GS65 <jeffreylee522@gmail.com>
2019-10-14 11:55:05 +0800

    EnvironmentToken

---
+++ src/EnvironmentToken.java
@@ -0,0 +1,10 @@
+public class EnvironmentToken {
+
+    private final String environmentToken = "CX";
+    private final String test = "hello";
+
+    public String getEnvironmentToken() {
+        System.out.println("Hello");
+        return environmentToken;
+    }
+}
